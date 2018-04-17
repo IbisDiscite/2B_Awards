d499c0dfa4f44d650590043187e3a69c2b10fbd5
Pedro Berrio <pcberriop@unal.edu.co>
2018-04-16 20:53:04 -0500

    puerto mongo index js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 //DB setup
-mongoose.connect("mongodb://mongo:27017");
+mongoose.connect("mongodb://mongo:3005");
     app.get('/', function(req, res){
 res.send("Hello World-changed-1 ");
 });
